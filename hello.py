47654319559932f35c76de3642747c5417d13713
tigo11 <tigrulia_442@mail.ru>
2025-11-30 18:36:11 -0500

    patch2: restyle hello.py

--- hello.py
+++ hello.py
@@ -4,16 +4,32 @@ import typer
 
 def main(
     name: str,
-    lastname: str = typer.Option("", help="Фамилия пользователя."),
-    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+
+    lastname: str = typer.Option(
+	"", 
+	help="Фамилия пользователя."
+    ),
+
+    formal: bool = typer.Option(
+	False, 
+	"--formal", 
+	"-f", 
+	help="Использовать формальное приветствие."
+    ),
 ):
+
     """
     Say Hello пользователю, опционально используя фамилию и формальный стиль.
     """
+
     if formal:
+
         print(f"Добрый день, {name} {lastname}!")
+
     else:
+
         print(f"Привет, {name}!")
 
 if __name__ == "__main__":
+
     typer.run(main)
