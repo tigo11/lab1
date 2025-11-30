3da1881957746a2cd9afb87fda61e22432e4955c
tigo11 <tigrulia_442@mail.ru>
2025-11-30 18:03:18 -0500

    Modernize hello.py (patch1)

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
-name = input("Enter name: ")
-print(f"Hello appsec world from @{name}")
+import typer
+
+def main(
+    name: str,
+    lastname: str = typer.Option("", help="Фамилия пользователя."),
+    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+):
+    """
+    Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
+    """
+    if formal:
+        print(f"Добрый день, {name} {lastname}!")
+    else:
+        print(f"Привет, {name}!")
+
+if __name__ == "__main__":
+    typer.run(main)
